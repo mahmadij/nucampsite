dd48d4b3ed74411b0bc8e46efc74e1eac05c96d4
Mehrnaz <mahmadij@gmail.com>
2020-09-17 23:01:53 -0500

    End of week 4 assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
         $(function() {
-            $(".carousel").carousel( { interval: 2000 } );
+            $(".carousel").carousel( { interval: 2000,pause:false } );
             $("#carouselButton").click(function(){
                 if ($("#carouselButton").children("i").hasClass("fa-pause")) {
                     $(".carousel").carousel("pause");
@@ -10,5 +10,11 @@
                     $("#carouselButton").children("i").removeClass("fa-play");
                     $("#carouselButton").children("i").addClass("fa-pause"); 
                 }
+            });
+            $("#reserveButton").click(function(){
+                $("#reserveModal").modal("show");
+            });
+            $("#loginButton").click(function(){
+                $("#loginModal").modal("show");
             });
         });
